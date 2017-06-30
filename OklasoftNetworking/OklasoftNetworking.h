050e6df6c77f4d442d7feeb141926ddc6c2c356f
Justin Oakes <justin@justins-macbook-pro.local>
2017-06-29 19:05:52 -0500

    Initial Commit

---
+++ OklasoftNetworking/OklasoftNetworking.h
@@ -0,0 +1,19 @@
+//
+//  OklasoftNetworking.h
+//  OklasoftNetworking
+//
+//  Created by Justin Oakes on 6/29/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OklasoftNetworking.
+FOUNDATION_EXPORT double OklasoftNetworkingVersionNumber;
+
+//! Project version string for OklasoftNetworking.
+FOUNDATION_EXPORT const unsigned char OklasoftNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OklasoftNetworking/PublicHeader.h>
+
+
